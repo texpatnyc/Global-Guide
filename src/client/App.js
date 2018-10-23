5a86efc2ba7e26061d87c1d165d248b48f18d19d
Kyle Combs <combskyle@gmail.com>
2018-10-23 14:27:01 -0400

    added image

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './app.css';
-import ReactImage from './react.png';
+import WooHoo from './react.png';
 
 export default class App extends Component {
   state = { username: null };
@@ -16,8 +16,8 @@ export default class App extends Component {
     return (
       <div>
         {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+        <img src={WooHoo} alt="react" width="100%"/>
       </div>
     );
-  }
+  } 
 }
