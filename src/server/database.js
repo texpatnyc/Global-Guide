74ebb5098549d2220a3128902b41649582d881cb
Kyle Combs <combskyle@gmail.com>
2018-10-24 00:17:05 -0400

    full CRUD functionality for Locales and Restaurants

--- src/server/database.js
+++ src/server/database.js
@@ -1,19 +1,60 @@
-'use strict';
-
 const Sequelize = require('sequelize');
-
-// const sequelize = new Sequelize('global_guide', 'texpatnyc', 'nomames', {
-//   host: 'localhost:5432',
-//   dialect: 'postgres'
-// });
 
 const sequelize = new Sequelize('postgres://texpatnyc:nomames@localhost:5432/global_guide');
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  })
+
+// Models
+
+const Locale = sequelize.define('locale', {
+  id: { type: Sequelize.UUID, primaryKey: true, defaultValue: Sequelize.UUIDV4, allowNull: false },
+  cityName: { type: Sequelize.STRING, allowNull: false },
+  summary: { type: Sequelize.TEXT, allowNull: false }
+})
+
+const Restaurant = sequelize.define('restaurant', {
+  id: { type: Sequelize.UUID, primaryKey: true, defaultValue: Sequelize.UUIDV4, allowNull: false },
+  cityId: { type: Sequelize.UUID, allowNull: false },
+  name: { type: Sequelize.STRING, allowNull: false },
+  address: { type: Sequelize.STRING },
+  url: { type: Sequelize.STRING, },
+  description: { type: Sequelize.TEXT, allowNull: false }
+})
+
+const Bar = sequelize.define('bar', {
+  id: { type: Sequelize.UUID, primaryKey: true, defaultValue: Sequelize.UUIDV4, allowNull: false },
+  cityId: { type: Sequelize.UUID, allowNull: false },
+  name: { type: Sequelize.STRING, allowNull: false },
+  address: { type: Sequelize.STRING },
+  url: { type: Sequelize.STRING, },
+  description: { type: Sequelize.TEXT, allowNull: false }
+})
+
+const Sight = sequelize.define('sight', {
+  id: { type: Sequelize.UUID, primaryKey: true, defaultValue: Sequelize.UUIDV4, allowNull: false },
+  cityId: { type: Sequelize.UUID, allowNull: false },
+  name: { type: Sequelize.STRING, allowNull: false },
+  address: { type: Sequelize.STRING },
+  url: { type: Sequelize.STRING, },
+  description: { type: Sequelize.TEXT, allowNull: false }
+})
+
+// Associations 
+
+
+// Open DB
+
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   })
+
+//   Locale.create({
+//     cityName: 'Austin',
+//     summary: "The city of my childhood!"
+//   });
+
+  module.exports = { Locale, Restaurant, Bar, Sight };
---
+++ src/server/server.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const os = require('os');
+const bodyParser = require('body-parser');
+
+const { Locale, Restaurant, Bar, Sight } = require('./database');
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('postgres://texpatnyc:nomames@localhost:5432/global_guide');
+
+
+const app = express();
+
+app.use(express.static('dist'));
+
+app.use(bodyParser.json());
+
+app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/api/locales', (req, res) => {
+  Locale.findAll({ attributes: ['cityName'] })
+    .then( (data) => { res.json(data) });
+})
+
+app.post('/api/locales', (req, res) => {
+  console.log(req.body);
+  const { cityName, summary } = req.body;
+  Locale.create({ cityName: cityName, summary: summary })
+    .then( console.log('Locale Added, BIATCH!') )
+    .then( res.end() )
+})
+
+app.get('/api/restaurants/:cityId', (req, res) => {
+  const { cityId } = req.params;
+  Restaurant.findAll({ attributes: ['name', 'address', 'url'] }, { where: { cityId: cityId } })
+    .then( (data) => { res.json(data) } );
+})
+
+app.post('/api/restaurants/:cityId', (req, res) => {
+  const { cityId } = req.params;
+  const { name, address, url, description } = req.body;
+  Restaurant.create({
+    cityId: cityId,
+    name: name,
+    address: address,
+    url: url,
+    description: description
+  })
+    .then( console.log('Restaurant added, BIATCH!!!!') )
+    .then( res.end() )
+})
+
+app.put('/api/restaurants/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+		const message = (
+			`Request path id (${req.params.id}) and request body id ` +
+			`(${req.body.id}) must match`);
+		req.flash('error', message);
+		return res.redirect('back');
+  }
+
+  const toUpdate = {};
+	const updateableFields = ['name', 'address', 'url', 'description'];
+
+	updateableFields.forEach(field => {
+		if (field in req.body) {
+			toUpdate[field] = req.body[field];
+		}
+  });
+  const keys = Object.keys(toUpdate);
+  console.log(toUpdate);
+  console.log(keys);
+
+  Restaurant.update(toUpdate, { where: { id: req.params.id }, fields: keys })
+    .then( () => {
+      console.log('successful edit');
+      res.end()
+    })
+})
+
+app.delete('/api/restaurants/:id', (req, res) => {
+  Restaurant.destroy( { where: { id: req.params.id } })
+    .then(() => {
+      console.log('restaurant deleted');
+      res.end();
+    })
+  })
+
+
+
+
+app.listen(8080, () => console.log('Listening on port 8080!'));
